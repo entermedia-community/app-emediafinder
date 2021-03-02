59878df9d3c57ee1658655dc5bb73eb3ab5f464d
Nikhil <finestcode@gmail.com>
2021-03-02 16:57:06 +0530

    AppDelegate.swift changes

--- em_mobile_flutter/ios/Runner/AppDelegate.swift
+++ em_mobile_flutter/ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import FirebaseCore
+import flutter_downloader
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -13,4 +14,10 @@ import FirebaseCore
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+
+private func registerPlugins(registry: FlutterPluginRegistry) {
+    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+       FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
+    }
 }
