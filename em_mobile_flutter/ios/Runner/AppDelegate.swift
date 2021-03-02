d1e036b0ee508c57adfd226274b2f9058e1478d4
finestcode <finestcode@gmail.com>
2021-03-02 11:41:47 -0500

    Changes for file downloader

--- em_mobile_flutter/ios/Runner/AppDelegate.swift
+++ em_mobile_flutter/ios/Runner/AppDelegate.swift
@@ -5,19 +5,22 @@ import flutter_downloader
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
+    if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+     }
     GeneratedPluginRegistrant.register(with: self)
-    [FlutterDownloaderPlugin setPluginRegistrantCallback:registerPlugins];
+    FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
 
-void registerPlugins(NSObject<FlutterPluginRegistry>* registry) {
-  // [GeneratedPluginRegistrant registerWithRegistry:registry];
-    [FlutterDownloaderPlugin registerWithRegistrar:[registry registrarForPlugin:@"vn.hunghd.flutter_downloader"]];
+private func registerPlugins(registry: FlutterPluginRegistry) {
+    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+       FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
+    }
 }
